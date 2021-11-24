d5112f09e216dad09f317d5f35f4c392245fc4a1
ZhangRui111 <zhangruisg111@163.com>
2021-11-24 20:36:02 +0800

    Update MyGAN

--- MyGAN/simpleGAN/run_simpleGAN.py
+++ MyGAN/simpleGAN/run_simpleGAN.py
@@ -7,184 +7,184 @@ import torch.optim as optim
 from torchvision import datasets
 import torchvision.transforms as transforms
 
-from discri import Discriminator
-from genera import Generator
-
-# number of subprocesses to use for data loading
-num_workers = 0
-# how many samples per batch to load
-batch_size = 64
-# convert data to torch.FloatTensor
-transform = transforms.ToTensor()
-# get the training datasets
-train_data = datasets.MNIST(root='../data', train=True, download=True, transform=transform)
-# prepare data loader
-train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, num_workers=num_workers)
-
-# # obtain one batch of training images
-# dataiter = iter(train_loader)
-# images, labels = dataiter.next()
-# images = images.numpy()
-
-# # get one image from the batch for visualization
-# img = np.squeeze(images[0])
-# fig = plt.figure(figsize=(3, 3))
-# ax = fig.add_subplot(111)
-# ax.imshow(img, cmap='gray')
-# plt.show()
-
-# Discriminator hyperparams
-# Size of input image to discriminator (28*28)
-input_size = 784
-# Size of discriminator output (real or fake)
-d_output_size = 1
-# Size of last hidden layer in the discriminator
-d_hidden_size = 32
-
-# Generator hyperparams
-# Size of latent vector to give to generator
-z_size = 100
-# Size of discriminator output (generated image)
-g_output_size = 784
-# Size of first hidden layer in the generator
-g_hidden_size = 32
-
-# instantiate discriminator and generator
-D = Discriminator(input_size, d_hidden_size, d_output_size)
-G = Generator(z_size, g_hidden_size, g_output_size)
+from discriminator import Discriminator
+from generator import Generator
+from vis import view_samples
 
 
-# Calculate losses
-def real_loss(D_out, smooth=False):
-    batch_size = D_out.size(0)
+def real_loss(criterion, preds, labels, smooth=False):
     # label smoothing
     if smooth:
-        # smooth, real labels = 0.9
-        labels = torch.ones(batch_size) * 0.9
-    else:
-        labels = torch.ones(batch_size)  # real labels = 1
+        labels = labels * 0.9
 
-    # numerically stable loss
-    criterion = nn.BCEWithLogitsLoss()
-    # calculate loss
-    loss = criterion(D_out.squeeze(), labels)
+    loss = criterion(preds.squeeze(), labels)
     return loss
 
 
-def fake_loss(D_out):
-    batch_size = D_out.size(0)
-    labels = torch.zeros(batch_size)  # fake labels = 0
-    criterion = nn.BCEWithLogitsLoss()
-    # calculate loss
-    loss = criterion(D_out.squeeze(), labels)
+def fake_loss(criterion, preds, labels):
+    loss = criterion(preds.squeeze(), labels)
     return loss
 
-# Optimizers
-lr = 0.002
-# training hyperparams
-num_epochs = 100
-# keep track of loss and generated, "fake" samples
-samples = []
-losses = []
 
-print_every = 400
+def set_model_gradient(model, requires_grad):
+    for param in model.parameters():
+        param.requires_grad = requires_grad
 
-# Get some fixed data for sampling. These are images that are held
-# constant throughout training, and allow us to inspect the model's performance
-sample_size = 16
-fixed_z = np.random.uniform(-1, 1, size=(sample_size, z_size))
-fixed_z = torch.from_numpy(fixed_z).float()
 
-# Create optimizers for the discriminator and generator
-d_optimizer = optim.Adam(D.parameters(), lr)
-g_optimizer = optim.Adam(G.parameters(), lr)
+def main():
+    # # -------------------- Data --------------------
+    num_workers = 8  # number of subprocesses to use for data loading
+    batch_size = 64  # how many samples per batch to load
+    transform = transforms.ToTensor()  # convert data to torch.FloatTensor
+    train_data = datasets.MNIST(root='../data', train=True, download=True, transform=transform)
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, num_workers=num_workers)
 
-# train the network
-D.train()
-G.train()
-for epoch in range(num_epochs):
-    for batch_i, (real_images, _) in enumerate(train_loader):
-        batch_size = real_images.size(0)
-        ## Important rescaling step ##
-        real_images = real_images * 2 - 1  # rescale input images from [0,1) to [-1, 1)
+    # # Obtain one batch of training images
+    # dataiter = iter(train_loader)
+    # images, labels = dataiter.next()
+    # images = images.numpy()
+    # # Get one image from the batch for visualization
+    # img = np.squeeze(images[0])
+    # fig = plt.figure(figsize=(3, 3))
+    # ax = fig.add_subplot(111)
+    # ax.imshow(img, cmap='gray')
+    # plt.show()
 
-        # ============================================
-        #            TRAIN THE DISCRIMINATOR
-        # ============================================
+    # # -------------------- Discriminator and Generator --------------------
+    # Discriminator hyperparams
+    input_size = 784  # Size of input image to discriminator (28*28)
+    d_output_size = 1  # Size of discriminator output (real or fake)
+    d_hidden_size = 32  # Size of last hidden layer in the discriminator
+    # Generator hyperparams
+    z_size = 100  # Size of latent vector to give to generator
+    g_output_size = 784  # Size of discriminator output (generated image)
+    g_hidden_size = 32  # Size of first hidden layer in the generator
+    # Instantiate discriminator and generator
+    D = Discriminator(input_size, d_hidden_size, d_output_size)
+    G = Generator(z_size, g_hidden_size, g_output_size)
 
-        d_optimizer.zero_grad()
+    # # -------------------- Optimizers and Criterion --------------------
+    # Training hyperparams
+    num_epochs = 100
+    print_every = 400
+    lr = 0.002
 
-        # 1. Train with real images
+    # Create optimizers for the discriminator and generator, respectively
+    d_optimizer = optim.Adam(D.parameters(), lr)
+    g_optimizer = optim.Adam(G.parameters(), lr)
+    losses = []  # keep track of generated "fake" samples
 
-        # Compute the discriminator losses on real images
-        # smooth the real labels
-        D_real = D(real_images)
-        d_real_loss = real_loss(D_real, smooth=True)
+    criterion = nn.BCEWithLogitsLoss()
 
-        # 2. Train with fake images
+    # -------------------- Training --------------------
+    D.train()
+    G.train()
 
-        # Generate fake images
-        z = np.random.uniform(-1, 1, size=(batch_size, z_size))
-        z = torch.from_numpy(z).float()
-        fake_images = G(z)
+    # Get some fixed data for sampling. These are images that are held
+    # constant throughout training, and allow us to inspect the model's performance
+    sample_size = 16
+    fixed_z = np.random.uniform(-1, 1, size=(sample_size, z_size))
+    fixed_z = torch.from_numpy(fixed_z).float()
+    samples = []  # keep track of loss
 
-        # Compute the discriminator losses on fake images
-        D_fake = D(fake_images)
-        d_fake_loss = fake_loss(D_fake)
+    for epoch in range(num_epochs):
+        for batch_i, (real_images, _) in enumerate(train_loader):
+            batch_size = real_images.size(0)
 
-        # add up loss and perform backprop
-        d_loss = d_real_loss + d_fake_loss
-        d_loss.backward()
-        d_optimizer.step()
+            # Important rescaling step
+            real_images = real_images * 2 - 1  # rescale input images from [0,1) to [-1, 1)
 
-        # =========================================
-        #            TRAIN THE GENERATOR
-        # =========================================
-        g_optimizer.zero_grad()
+            # Generate fake images, used for both discriminator and generator
+            z = np.random.uniform(-1, 1, size=(batch_size, z_size))
+            z = torch.from_numpy(z).float()
+            fake_images = G(z)
 
-        # 1. Train with fake images and flipped labels
+            real_labels = torch.ones(batch_size)
+            fake_labels = torch.zeros(batch_size)
 
-        # Generate fake images
-        z = np.random.uniform(-1, 1, size=(batch_size, z_size))
-        z = torch.from_numpy(z).float()
-        fake_images = G(z)
+            # ============================================
+            #            TRAIN THE DISCRIMINATOR
+            # ============================================
 
-        # Compute the discriminator losses on fake images
-        # using flipped labels!
-        D_fake = D(fake_images)
-        g_loss = real_loss(D_fake)  # use real loss to flip labels
+            d_optimizer.zero_grad()
 
-        # perform backprop
-        g_loss.backward()
-        g_optimizer.step()
+            # 1. Train with real images
 
-        # Print some loss stats
-        if batch_i % print_every == 0:
-            # print discriminator and generator loss
-            print('Epoch [{:5d}/{:5d}] | d_loss: {:6.4f} | g_loss: {:6.4f}'.format(
-                epoch + 1, num_epochs, d_loss.item(), g_loss.item()))
+            # Compute the discriminator losses on real images
+            D_real = D(real_images)
+            d_real_loss = real_loss(criterion, D_real, real_labels, smooth=True)
 
-    ## AFTER EACH EPOCH ##
-    # append discriminator loss and generator loss
-    losses.append((d_loss.item(), g_loss.item()))
+            # 2. Train with fake images
 
-    # generate and save sample, fake images
-    G.eval()  # eval mode for generating samples
-    samples_z = G(fixed_z)
-    samples.append(samples_z)
-    G.train()  # back to train mode
+            # Compute the discriminator losses on fake images
+            # -------------------------------------------------------
+            # ATTENTION:
+            # *.detach(), thus, generator is fixed when we optimize
+            # the discriminator
+            # -------------------------------------------------------
+            D_fake = D(fake_images.detach())
+            d_fake_loss = fake_loss(criterion, D_fake, fake_labels)
 
-torch.save(G.state_dict(), "G.pth")
-torch.save(D.state_dict(), "D.pth")
+            # 3. Add up loss and perform backprop
+            d_loss = (d_real_loss + d_fake_loss) * 0.5
+            d_loss.backward()
+            d_optimizer.step()
 
-# Save training generator samples
-with open('train_samples.pkl', 'wb') as f:
-    pkl.dump(samples, f)
+            # =========================================
+            #            TRAIN THE GENERATOR
+            # =========================================
 
-fig, ax = plt.subplots()
-losses = np.array(losses)
-plt.plot(losses.T[0], label='Discriminator')
-plt.plot(losses.T[1], label='Generator')
-plt.title("Training Losses")
-plt.legend()
-plt.show()
+            g_optimizer.zero_grad()
+
+            # Make the discriminator fixed when optimizing the generator
+            set_model_gradient(D, False)
+
+            # 1. Train with fake images and flipped labels
+
+            # Compute the discriminator losses on fake images using flipped labels!
+            G_D_fake = D(fake_images)
+            g_loss = real_loss(criterion, G_D_fake, real_labels)  # use real loss to flip labels
+
+            # 2. Perform backprop
+            g_loss.backward()
+            g_optimizer.step()
+
+            # Make the discriminator require_grad=True after optimizing the generator
+            set_model_gradient(D, True)
+
+            # =========================================
+            #           Print some loss stats
+            # =========================================
+            if batch_i % print_every == 0:
+                print('Epoch [{:5d}/{:5d}] | d_loss: {:6.4f} | g_loss: {:6.4f}'.format(
+                    epoch + 1, num_epochs, d_loss.item(), g_loss.item()))
+
+        # AFTER EACH EPOCH
+        losses.append((d_loss.item(), g_loss.item()))
+
+        # generate and save sample, fake images
+        G.eval()  # eval mode for generating samples
+        samples_z = G(fixed_z)
+        samples.append(samples_z)
+        view_samples(-1, samples, "last_sample.png")
+        G.train()  # back to train mode
+
+    # Save models and training generator samples
+    torch.save(G.state_dict(), "G.pth")
+    torch.save(D.state_dict(), "D.pth")
+    with open('train_samples.pkl', 'wb') as f:
+        pkl.dump(samples, f)
+
+    # Plot the loss curve
+    fig, ax = plt.subplots()
+    losses = np.array(losses)
+    plt.plot(losses.T[0], label='Discriminator')
+    plt.plot(losses.T[1], label='Generator')
+    plt.title("Training Losses")
+    plt.legend()
+    plt.savefig("loss.png")
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
--- MyGAN/simpleGAN/vis.py
+++ MyGAN/simpleGAN/vis.py
@@ -3,58 +3,65 @@ import numpy as np
 import pickle as pkl
 import torch
 
-from genera import Generator
+from generator import Generator
 
 
 # helper function for viewing a list of passed in sample images
-def view_samples(epoch, samples):
+def view_samples(epoch, samples, file_name=None, show=False):
     fig, axes = plt.subplots(figsize=(7, 7), nrows=4, ncols=4, sharey=True, sharex=True)
     for ax, img in zip(axes.flatten(), samples[epoch]):
         img = img.detach()
         ax.xaxis.set_visible(False)
         ax.yaxis.set_visible(False)
         im = ax.imshow(img.reshape((28, 28)), cmap='Greys_r')
+    if file_name:
+        plt.savefig(file_name)
+    if show:
+        plt.show()
+    plt.close()
+
+
+def main():
+    # Load samples from generator, taken while training
+    with open('train_samples.pkl', 'rb') as f:
+        samples = pkl.load(f)
+
+    # View a sample from the final epoch, -1 indicates final epoch
+    view_samples(-1, samples, "training_last_epoch.png")
+
+    rows = 10  # split epochs into 10, so 100/10 = every 10 epochs
+    cols = 6
+    fig, axes = plt.subplots(figsize=(7, 12), nrows=rows, ncols=cols, sharex=True, sharey=True)
+    for sample, ax_row in zip(samples[::int(len(samples) / rows)], axes):
+        for img, ax in zip(sample[::int(len(sample) / cols)], ax_row):
+            img = img.detach()
+            ax.imshow(img.reshape((28, 28)), cmap='Greys_r')
+            ax.xaxis.set_visible(False)
+            ax.yaxis.set_visible(False)
+    plt.savefig("training_epochs.png")
     plt.show()
+    plt.close()
+
+    # Generator hyperparams
+    z_size = 100
+    g_output_size = 784
+    g_hidden_size = 32
+    G = Generator(z_size, g_hidden_size, g_output_size)
+    G.load_state_dict(torch.load("G.pth"))
+    G.eval()  # eval mode
+
+    # # Sampling from the generator
+    # randomly generated, new latent vectors
+    sample_size = 16
+    rand_z = np.random.uniform(-1, 1, size=(sample_size, z_size))
+    rand_z = torch.from_numpy(rand_z).float()
+    # generated samples
+    rand_images = G(rand_z)
+
+    # 0 indicates the first set of samples in the passed in list
+    # and we only have one batch of samples, here
+    view_samples(0, [rand_images], "resample.png")
 
 
-# Load samples from generator, taken while training
-with open('train_samples.pkl', 'rb') as f:
-    samples = pkl.load(f)
-
-# -1 indicates final epoch's samples (the last in the list)
-view_samples(-1, samples)
-
-rows = 10  # split epochs into 10, so 100/10 = every 10 epochs
-cols = 6
-fig, axes = plt.subplots(figsize=(7, 12), nrows=rows, ncols=cols, sharex=True, sharey=True)
-for sample, ax_row in zip(samples[::int(len(samples) / rows)], axes):
-    for img, ax in zip(sample[::int(len(sample) / cols)], ax_row):
-        img = img.detach()
-        ax.imshow(img.reshape((28, 28)), cmap='Greys_r')
-        ax.xaxis.set_visible(False)
-        ax.yaxis.set_visible(False)
-plt.show()
-
-# Generator hyperparams
-# Size of latent vector to give to generator
-z_size = 100
-# Size of discriminator output (generated image)
-g_output_size = 784
-# Size of first hidden layer in the generator
-g_hidden_size = 32
-
-# # Sampling from the generator
-# randomly generated, new latent vectors
-sample_size = 16
-rand_z = np.random.uniform(-1, 1, size=(sample_size, z_size))
-rand_z = torch.from_numpy(rand_z).float()
-
-G = Generator(z_size, g_hidden_size, g_output_size)
-G.load_state_dict(torch.load("G.pth"))
-G.eval()  # eval mode
-# generated samples
-rand_images = G(rand_z)
-
-# 0 indicates the first set of samples in the passed in list
-# and we only have one batch of samples, here
-view_samples(0, [rand_images])
+if __name__ == '__main__':
+    main()
